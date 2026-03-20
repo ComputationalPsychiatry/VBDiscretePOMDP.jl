0fcf35f8b8079756b6ea090d59d49d32f1c12427
Documenter.jl <documenter@juliadocs.github.io>
2026-03-20 11:09:42 +0000

    build based on ca8967c

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[]
+[{"location":"#Variational-Bayes-Discrete-POMDP-(VBDiscretePOMDP)","page":"Home","title":"Variational Bayes Discrete POMDP (VBDiscretePOMDP)","text":"(Image: Dev)","category":"section"},{"location":"#Getting-started","page":"Home","title":"Getting started","text":"Install using Julia's package manager:\n\npkg> add VBDiscretePOMDP","category":"section"},{"location":"#Minimal-example","page":"Home","title":"Minimal example","text":"","category":"section"},{"location":"#Other-things","page":"Home","title":"Other things","text":"","category":"section"}]
 }
