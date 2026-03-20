7f26d47c7539d8cbc2adb7712c094017fcffd911
samuelnehrer02 <nehrer.samuel@gmail.com>
2026-03-20 12:02:12 +0100

    try docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,1 +1,41 @@
 using Documenter
+
+open(joinpath(joinpath(@__DIR__, "src"), "index.md"), "w") do io
+    println(
+        io,
+        """
+```@meta
+        EditURL = "https://github.com/ComputationalPsychiatry/VBDiscretePOMDP.jl/blob/main/README.md"
+```
+        """,
+    )
+    for line in eachline(joinpath(dirname(@__DIR__), "README.md"))
+        println(io, line)
+    end
+end
+
+pages = [
+    "Home" => "index.md",
+]
+
+fmt = Documenter.HTML(;
+    prettyurls=get(ENV, "CI", "false") == "true",
+    repolink="https://github.com/ComputationalPsychiatry/VBDiscretePOMDP.jl",
+    canonical="https://ComputationalPsychiatry.github.io/VBDiscretePOMDP.jl",
+    assets=String[],
+    collapselevel=1,
+    description="A Julia package for VBDiscretePOMDP",
+)
+
+makedocs(;
+    sitename="VBDiscretePOMDP.jl",
+    authors="Your Name",
+    format=fmt,
+    pages=pages,
+    pagesonly=true,
+)
+
+deploydocs(;
+    repo="github.com/ComputationalPsychiatry/VBDiscretePOMDP.jl.git",
+    devbranch="main",
+)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,4 @@
+using Test
 using Aqua
 using JET
 using VBDiscretePOMDP
